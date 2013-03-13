a8c3a5409625d623eb85c7cb22c7dbc100524dec
Marcelino de la Cruz Rot <marcelino.delacruz@upm.es>
2013-03-13 00:00:00 +0000

    version 0.0-5

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.First.lib <- function(lib, pkg)  {
-    library.dynam("dixon", pkg, lib)
-    packageStartupMessage("This is an improved (accelerated) version of function dixon, as requested by P.M. Jorgensen. This is ver.  ",
-                          utils::packageDescription("dixon", field="Version"),
-                          appendLF = TRUE)
-}
